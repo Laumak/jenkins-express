70668b9db7da2f28970f7616d4670f9639b2ed32
Lauri Mäkinen <latemak@gmail.com>
2017-03-04 01:26:06 +0200

    testing Github hooks to jenkins.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World asdads!");
 });
 
 app.listen(port, () => {
