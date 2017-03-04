08af468a157300f8c4813b61a536cbb40e3b99ca
Lauri Mäkinen <latemak@gmail.com>
2017-03-04 10:14:15 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-cd /var/www/html/jenkins-express \
-git pull \
-npm install --production \
-pm2 restart all \
+# cd /var/www/html/jenkins-express \
+# git pull \
+# npm install --production \
+# pm2 restart all \
+
+echo 'test'
